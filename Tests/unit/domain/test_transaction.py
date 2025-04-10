1505838413e8af0d62416c69a6576477c60bd4bf
herman katende <hermankats16@gmail.com>
2025-04-10 23:26:50 +0300

    implemented transaction-test

--- Tests/unit/domain/test_transaction.py
+++ Tests/unit/domain/test_transaction.py
@@ -1,5 +1,6 @@
 from domain.entities.transaction import Transaction, TransactionType
 
+
 def test_transaction_creation():
     txn = Transaction("txn1", "acc1", 100.0, TransactionType.DEPOSIT)
     assert txn.amount == 100.0
