8b6aef8f3db5ab5e0e8917fb83eb7a43ba6e9caf
rania Nabatanzi <ranianabatanzi004@gmail.com>
2025-04-16 13:07:52 +0300

    added logging_service

---
+++ application/services/logging_service.py
@@ -0,0 +1,18 @@
+# application/services/logging_service.py
+import logging
+
+logging.basicConfig(filename='transactions.log', level=logging.INFO)
+
+def log_transaction(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        transaction = result if isinstance(result, Transaction) else None
+        if transaction:
+            logging.info(
+                f"{transaction.type.value}: "
+                f"Amount: {transaction.amount}, "
+                f"Source: {transaction.source_account_id}, "
+                f"Destination: {transaction.destination_account_id or 'N/A'}"
+            )
+        return result
+    return wrapper
