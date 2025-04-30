6c21da8113388f0dd1e466d820980e67956dca30
laura Christine <lhaurah313@gmail.com>
2025-04-30 07:37:49 +0300

    modified  acc_creation_services

--- application/services/account_creation_services.py
+++ application/services/account_creation_services.py
@@ -1,16 +1,6 @@
 # from uuid import uuid4
 # from domain.entities.account import Account, SavingsAccount, CheckingAccount, AccountType
 
-# class AccountCreationService:
-#     def __init__(self, account_repository):
-#         self.account_repo = account_repository
-#         self.min_savings_deposit = 100.0
-
-#     def create_account(self, account_type: AccountType, initial_deposit: float = 0.0) -> str:
-#         if account_type == AccountType.SAVINGS and initial_deposit < self.min_savings_deposit:
-#             raise ValueError(f"Minimum initial deposit for savings is {self.min_savings_deposit}")
-
-
 from uuid import uuid4
 from domain.entities.account import Account, SavingsAccount, CheckingAccount
 
