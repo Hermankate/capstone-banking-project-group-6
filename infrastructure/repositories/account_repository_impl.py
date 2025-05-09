594de3c8af13f2b35731ef9f61437974d1e0ce97
laura Christine <lhaurah313@gmail.com>
2025-05-09 07:55:22 +0300

    working on repo implementations

--- infrastructure/repositories/account_repository_impl.py
+++ infrastructure/repositories/account_repository_impl.py
@@ -1,11 +1,22 @@
-# infrastructure/repositories/account_repository_impl.py
-from application.interfaces.account_repository import AccountRepository
-from domain.entities.account import Account  # Only import Account
 
-class InMemoryAccountRepository(AccountRepository):
-    def __init__(self):
-        self.accounts = {}
 
-    def create_account(self, account: Account) -> str:
-        self.accounts[account.account_id] = account
-        return account.account_id
+
+
+
+
+
+
+
+
+
+# # infrastructure/repositories/account_repository_impl.py
+# from application.interfaces.account_repository import AccountRepository
+# from domain.entities.account import Account  # Only import Account
+
+# class InMemoryAccountRepository(AccountRepository):
+#     def __init__(self):
+#         self.accounts = {}
+
+#     def create_account(self, account: Account) -> str:
+#         self.accounts[account.account_id] = account
+#         return account.account_id
