ae18df3593fdf4ad8d048e0ef3687d7122588512
laura Christine <lhaurah313@gmail.com>
2025-05-09 07:55:35 +0300

    working on repo implementations

--- infrastructure/repositories/account_repository_impl.py
+++ infrastructure/repositories/account_repository_impl.py
@@ -1,4 +1,24 @@
+from typing import Dict, Optional
+from domain.entities.account import Account
+from application.interfaces.account_repository import AccountRepository
 
+class InMemoryAccountRepository(AccountRepository):
+    def __init__(self):
+        self.accounts: Dict[str, Account] = {}
+        self.next_id = 1  # Simple ID generator
+
+    def create_account(self, account: Account) -> str:
+        account.account_id = str(self.next_id)
+        self.accounts[account.account_id] = account
+        self.next_id += 1
+        return account.account_id
+
+    def get_account_by_id(self, account_id: str) -> Optional[Account]:
+        return self.accounts.get(account_id)
+
+    def update_account(self, account: Account) -> None:
+        if account.account_id in self.accounts:
+            self.accounts[account.account_id] = account
 
 
 
